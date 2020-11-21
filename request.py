242e2c75f87831f77b214dadc52a644e83b1a400
chiro <sachinsinghroxx@gmail.com>
2020-11-21 15:11:24 +0530

    changed time out

--- request.py
+++ request.py
@@ -4,7 +4,7 @@ from pythonping import ping
 def checkPing(IP):
     IP=IP.split('//')[-1]
     try:
-        a=(ping(IP))
+        a=(ping(IP,timeout=1,count=2))
     except:
         return 0
     a = list(a)
