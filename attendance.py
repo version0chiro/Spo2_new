73ac06e58fea307f015a07a32fa5410720eb0f85
Chiro <sachinsinghroxx@gmail.com>
2020-12-24 17:25:36 +0530

    removing add unkown to the list for experimental

--- attendance.py
+++ attendance.py
@@ -109,12 +109,13 @@ def checkName(name,spo2,hr,Compensated,Ambient):
             pass
                         
     elif name != 'Unknown':
-        atte = unknown_to_list(name,atte,details,spo2,hr,Compensated,Ambient)
-        xl = pd.ExcelFile('excel_sheets/attendance.xlsx')
-        today = date.today()
-        if str(today) in xl.sheet_names:
-            append_df_to_excel('excel_sheets/attendance.xlsx',atte,sheet_name=str(today))
-        else:
-            with pd.ExcelWriter('excel_sheets/attendance.xlsx',mode='a') as writer:
-                atte.to_excel(writer,sheet_name=str(today))
+        pass
+        # atte = unknown_to_list(name,atte,details,spo2,hr,Compensated,Ambient)
+        # xl = pd.ExcelFile('excel_sheets/attendance.xlsx')
+        # today = date.today()
+        # if str(today) in xl.sheet_names:
+        #     append_df_to_excel('excel_sheets/attendance.xlsx',atte,sheet_name=str(today))
+        # else:
+        #     with pd.ExcelWriter('excel_sheets/attendance.xlsx',mode='a') as writer:
+        #         atte.to_excel(writer,sheet_name=str(today))
         
