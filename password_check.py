091d8708180eac7694bae50d31c0e69e74f053bd
Chiro <sachinsinghroxx@gmail.com>
2021-01-03 22:32:04 +0530

    fixed_pymongo_speed

--- password_check.py
+++ password_check.py
@@ -7,6 +7,7 @@ import pymongo
 from getmac import get_mac_address as gma
 import pickle
 import re, uuid
+
 def checkPassword(password):
     passwd=b'pyqt543'
     salt = bcrypt.gensalt()
@@ -19,8 +20,7 @@ def checkPassword(password):
         return 0
     
 def check_Password(password):
-    
-    myclient = pymongo.MongoClient("mongodb+srv://admin-sachin:Sachin123@cluster0.pf7ee.mongodb.net/")
+    myclient = pymongo.MongoClient("mongodb://admin-sachin:Sachin123@cluster0-shard-00-00.pf7ee.mongodb.net:27017,cluster0-shard-00-01.pf7ee.mongodb.net:27017,cluster0-shard-00-02.pf7ee.mongodb.net:27017/spo2?ssl=true&replicaSet=atlas-x3z4ou-shard-0&authSource=admin&retryWrites=true&w=majority")
     mydb = myclient["spo2"]
     mycol = mydb["identities"]
     MAC = str(':'.join(re.findall('..', '%012x' % uuid.getnode())))
@@ -28,25 +28,25 @@ def check_Password(password):
     
     query = {"MAC": MAC}
     try:
-        search = mycol.find(query)[0]
+        search = mycol.find_one(query)
         activationFromServer = search["ActivationKey"]
+        dateFromServer = search["updated_at"].date()
+        
     except:
         return [3,0]
   
     print("found in mongoDB")
   
     try:
-        if os.path.isfile("password/date.p"):
-            
-            expire_date=pickle.load( open( "password/date.p", "rb" ))
-            today = date.today()
-            difftime = expire_date - today
-            
-            if difftime > datetime.timedelta(days=0):
-                if str(activationFromServer)==str(password):
-                    return [1,difftime.days]
-                else:
-                    return [0,difftime.days]
+        today = date.today()
+        difftime = dateFromServer - today
+        difftime = difftime.days
+        print(difftime)
+        if difftime > 0:
+            if str(activationFromServer)==str(password):
+                return [1,difftime.days]
+            else:
+                return [0,difftime.days]
         else:
             return [2,0]
     except FileNotFoundError:
